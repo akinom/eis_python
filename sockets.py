b98e383188b562c748edec3b0b5fe58f5887bdd0
Monika Mevenkamp <monikam@princeton.edu>
2020-02-28 15:20:43 -0500

    add timeout

--- sockets.py
+++ sockets.py
@@ -3,6 +3,7 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.settimeout(1)
 result = sock.connect_ex(('127.0.0.1', 80))
 if result == 0:
     print("Port is open")
